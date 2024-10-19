20af402a9934c9316620debf83a06d8c1ed75845
Dynia <piotr.dynia@gmail.com>
2024-10-19 21:19:08 +0200

    first unit tests setup

---
+++ app/src/test/java/com/example/chefmate/featureDetails/data/repository/FakeDetailsRepository.kt
@@ -0,0 +1,47 @@
+package com.example.chefmate.featureDetails.data.repository
+
+import com.example.chefmate.core.data.api.dto.RecipeDetails
+import com.example.chefmate.featureDetails.domain.model.IngredientEntity
+import com.example.chefmate.featureDetails.domain.model.RecipeEntity
+import com.example.chefmate.featureDetails.domain.model.RecipeWithIngredients
+import com.example.chefmate.featureDetails.domain.repository.DetailsRepository
+
+class FakeDetailsRepository : DetailsRepository {
+
+    private val cachedRecipes = mutableListOf<RecipeWithIngredients>()
+    private var recipeFromAPI: RecipeDetails? = null
+
+    override suspend fun getRecipeDetailsFromAPI(id: Int): RecipeDetails {
+        return recipeFromAPI ?: throw IllegalStateException("Recipe not found in the fake API")
+    }
+
+    override suspend fun getRecipeFromCache(id: Int): RecipeWithIngredients {
+        return cachedRecipes.find { it.recipe.id == id }
+            ?: throw IllegalStateException("Recipe not found in the cache")
+    }
+
+    override suspend fun isRecipeBookmarked(recipeId: Int): Boolean {
+        return cachedRecipes.any { it.recipe.id == recipeId }
+    }
+
+    override suspend fun saveRecipeWithIngredients(
+        recipe: RecipeEntity,
+        ingredients: List<IngredientEntity>
+    ) {
+        cachedRecipes.removeIf { it.recipe.id == recipe.id }
+        cachedRecipes.add(
+            RecipeWithIngredients(
+                recipe = recipe,
+                ingredients = ingredients
+            )
+        )
+    }
+
+    override suspend fun deleteRecipeById(recipeId: Int) {
+        cachedRecipes.removeIf { it.recipe.id == recipeId }
+    }
+
+    fun createFakeRemoteRecipe(recipe: RecipeDetails) {
+        this.recipeFromAPI = recipe
+    }
+}
---
+++ app/src/test/java/com/example/chefmate/featureDetails/domain/usecase/CheckRecipeIsBookmarkedTest.kt
@@ -0,0 +1,57 @@
+package com.example.chefmate.featureDetails.domain.usecase
+
+import com.example.chefmate.featureDetails.data.repository.FakeDetailsRepository
+import com.example.chefmate.featureDetails.domain.model.RecipeEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class CheckRecipeIsBookmarkedTest {
+
+    private lateinit var repository: FakeDetailsRepository
+    private lateinit var checkRecipeIsBookmarked: CheckRecipeIsBookmarked
+
+    @Before
+    fun setUp() {
+        repository = FakeDetailsRepository()
+        checkRecipeIsBookmarked = CheckRecipeIsBookmarked(repository)
+    }
+
+    @Test
+    fun `Return true for existing recipe`() = runBlocking {
+        repository.saveRecipeWithIngredients(
+            recipe = RecipeEntity(
+                id = 1,
+                title = "Dummy title",
+                summary = "Dummy summary",
+                instructions = "Dummy instructions",
+                servings = 3,
+                readyInMinutes = 30,
+                aggregateLikes = 5,
+                imagePath = ""
+            ),
+            ingredients = emptyList()
+        )
+        assertTrue(checkRecipeIsBookmarked(1))
+    }
+
+    @Test
+    fun `Return false for non existing recipe`() = runBlocking {
+        repository.saveRecipeWithIngredients(
+            recipe = RecipeEntity(
+                id = 1,
+                title = "Dummy title",
+                summary = "Dummy summary",
+                instructions = "Dummy instructions",
+                servings = 3,
+                readyInMinutes = 30,
+                aggregateLikes = 5,
+                imagePath = ""
+            ),
+            ingredients = emptyList()
+        )
+        assertFalse(checkRecipeIsBookmarked(2))
+    }
+}
